babfb82f2594c4286189fffb0913b99b086b736c
DismissedLight <1686188646@qq.com>
2022-03-20 16:10:12 +0800

    Update Downloader.cs

--- Downloader.cs
+++ Downloader.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Net.Http;
@@ -10,8 +11,8 @@ namespace Snap.Net.Download
     public class Downloader
     {
         private const int BufferSize = 8192;
-        private readonly string _downloadUrl;
-        private readonly string _destinationFilePath;
+        private readonly Uri downloadUrl;
+        private readonly string destinationFilePath;
 
         // HttpClient is intended to be instantiated once per application, rather than per-use.
         private static readonly Lazy<HttpClient> LazyHttpClient = new(() => new() { Timeout = Timeout.InfiniteTimeSpan });
@@ -20,17 +21,15 @@ namespace Snap.Net.Download
 
         public event ProgressChangedHandler? ProgressChanged;
 
-        public Downloader(string downloadUrl, string destinationFilePath)
+        public Downloader(Uri uri, string destinationFilePath) 
         {
-            _downloadUrl = downloadUrl;
-            _destinationFilePath = destinationFilePath;
+            this.downloadUrl = uri;
+            this.destinationFilePath = destinationFilePath;
         }
-
-        public Downloader(Uri uri, string destinationFilePath) : this(uri.ToString(), destinationFilePath) { }
 
         public async Task DownloadAsync()
         {
-            using (HttpResponseMessage? response = await LazyHttpClient.Value.GetAsync(_downloadUrl, HttpCompletionOption.ResponseHeadersRead))
+            using (HttpResponseMessage response = await LazyHttpClient.Value.GetAsync(downloadUrl, HttpCompletionOption.ResponseHeadersRead))
             {
                 await DownloadFileFromHttpResponseMessageAsync(response);
             }
@@ -42,24 +41,24 @@ namespace Snap.Net.Download
 
             long? totalBytes = response.Content.Headers.ContentLength;
 
-            using (Stream? contentStream = await response.Content.ReadAsStreamAsync())
+            using (Stream contentStream = await response.Content.ReadAsStreamAsync())
             {
-                await ProcessContentStream(totalBytes, contentStream);
+                await ProcessContentStream(contentStream, totalBytes);
             }
         }
-        [SuppressMessage("", "CA1835")]
-        private async Task ProcessContentStream(long? totalDownloadSize, Stream contentStream)
+        
+        private async Task ProcessContentStream(Stream contentStream, long? totalDownloadSize)
         {
             long totalBytesRead = 0L;
             long readCount = 0L;
-            byte[]? buffer = new byte[BufferSize];
+            byte[] buffer = new byte[BufferSize];
             bool isMoreToRead = true;
 
-            using (FileStream? fileStream = new(_destinationFilePath, FileMode.Create, FileAccess.Write, FileShare.None, BufferSize, true))
+            using (FileStream fileStream = new(destinationFilePath, FileMode.Create, FileAccess.Write, FileShare.None, BufferSize, true))
             {
                 do
                 {
-                    int bytesRead = await contentStream.ReadAsync(buffer, 0, buffer.Length);
+                    int bytesRead = await contentStream.ReadAsync(buffer);
                     if (bytesRead == 0)
                     {
                         isMoreToRead = false;
@@ -67,12 +66,12 @@ namespace Snap.Net.Download
                         continue;
                     }
 
-                    await fileStream.WriteAsync(buffer, 0, bytesRead);
+                    await fileStream.WriteAsync(buffer.AsMemory(0, bytesRead));
 
                     totalBytesRead += bytesRead;
                     readCount += 1;
 
-                    if (readCount % 10 == 0)
+                    if (readCount % 8 == 0)
                     {
                         TriggerProgressChanged(totalDownloadSize, totalBytesRead);
                     }
